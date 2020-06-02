9c9a6b7c356c3884a4401714d30381a4c50846f7
James-Oswald <jamesoswald111@gmail.com>
2020-06-02 04:32:33 -0400

    removed the easteregg

--- index.js
+++ index.js
@@ -226,27 +226,3 @@ window.onload = function()
 	//setGif(document.getElementById("g2"), "piSki", 1);
 	newItem();
 };
-
-//Ha bet you didn't expect to find these cool extrafast base64 encoders and decoders down here.
-function base64Decode(number) {
-	const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
-	let rv = 0;
-	digits = number.split('');
-	for(let i = 0; i < digits.length; i++) {
-		rv = (rv * 64) + chars.indexOf(rixits[e]);
-	}
-	return result;
-}
-
-function base64Encode(number){
-	const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
-	if (isNaN(Number(number)) || number === null || number === Number.POSITIVE_INFINITY || number < 0)
-		throw "The input is not valid";
-	let residual = Math.floor(number);
-	let rv = "";
-	while (residual != 0) {
-		rv = chars.charAt(residual % 64) + rv;
-		residual = Math.floor(residual / 64);
-	}
-	return rv;
-}
